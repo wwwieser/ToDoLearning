ca69f9952c25ed3f45c4b30b54a9bc7299e48ab9
Weston Wieser <wes.wieser@accesscfa.com>
2019-11-14 12:19:24 -0500

    List + updates work

--- src/app/features/to-do-list/containers/to-do-list/to-do-list.component.ts
+++ src/app/features/to-do-list/containers/to-do-list/to-do-list.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ListComponent } from '../../components/list/list.component';
+import { AddNewItemComponent } from '../../components/add-new-item/add-new-item.component';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.scss']
+  styleUrls: ['./to-do-list.component.scss'],
+  providers: [
+    ListComponent,
+    AddNewItemComponent
+  ]
 })
 export class ToDoListComponent implements OnInit {
 
